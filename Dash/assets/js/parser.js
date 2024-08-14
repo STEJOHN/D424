d5d5162a0df40ea671b6e03c241b398626d10b3a
jas <jas@Mac.attlocal.net>
2024-08-13 22:39:19 -0500

    working test1

--- Dash/assets/js/parser.js
+++ Dash/assets/js/parser.js
@@ -117,17 +117,95 @@ export class TaskFactory {
 }
 
 // TaskManager class (demonstrates encapsulation)
+// export class TaskManager {
+//   constructor() {
+//     this._tasks = [];
+//     this._dc1 = [];
+//     this._dc2 = [];
+//     this._dc3 = [];
+//     this._dc4 = [];
+//     this._dc5 = [];
+//     this._dc6 = [];
+//     this._dc7 = [];
+//     this._dc8 = [];
+//   }
+
+//   addTask(taskData) {
+//     const task = TaskFactory.createTask(taskData);
+//     this._tasks.push(task);
+//     return task;
+//   }
+
+//   getResolvedTasks() {
+//     return this._tasks.filter((task) => task.isResolved());
+//   }
+
+//   getUnresolvedTasks() {
+//     return this._tasks.filter((task) => !task.isResolved());
+//   }
+
+//   get tasks() {
+//     return this._tasks.map((task) => task._data);
+//   }
+
+//   get dc1() {
+//     return this._dc1.map((task) => task._data);
+//   }
+
+//   get dc2() {
+//     return this._dc2.map((task) => task._data);
+//   }
+
+//   get dc3() {
+//     return this._dc3.map((task) => task._data);
+//   }
+
+//   get dc4() {
+//     return this._dc4.map((task) => task._data);
+//   }
+//   get dc5() {
+//     return this._dc5.map((task) => task._data);
+//   }
+//   get dc6() {
+//     return this._dc6.map((task) => task._data);
+//   }
+//   get dc7() {
+//     return this._dc7.map((task) => task._data);
+//   }
+//   get dc8() {
+//     return this._dc8.map((task) => task._data);
+//   }
+
+//   addToDc1(task) {
+//     this._dc1.push(task);
+//   }
+
+//   addToDc2(task) {
+//     this._dc2.push(task);
+//   }
+
+//   addToDc3(task) {
+//     this._dc3.push(task);
+//   }
+//   addToDc4(task) {
+//     this._dc4.push(task);
+//   }
+//   addToDc5(task) {
+//     this._dc5.push(task);
+//   }
+//   addToDc6(task) {
+//     this._dc6.push(task);
+//   }
+//   addToDc7(task) {
+//     this._dc7.push(task);
+//   }
+//   addToDc8(task) {
+//     this._dc8.push(task);
+//   }
+// }
 export class TaskManager {
   constructor() {
     this._tasks = [];
-    this._dc1 = [];
-    this._dc2 = [];
-    this._dc3 = [];
-    this._dc4 = [];
-    this._dc5 = [];
-    this._dc6 = [];
-    this._dc7 = [];
-    this._dc8 = [];
   }
 
   addTask(taskData) {
@@ -136,71 +214,12 @@ export class TaskManager {
     return task;
   }
 
-  getResolvedTasks() {
-    return this._tasks.filter((task) => task.isResolved());
-  }
-
-  getUnresolvedTasks() {
-    return this._tasks.filter((task) => !task.isResolved());
-  }
-
   get tasks() {
     return this._tasks.map((task) => task._data);
   }
 
-  get dc1() {
-    return this._dc1.map((task) => task._data);
-  }
-
-  get dc2() {
-    return this._dc2.map((task) => task._data);
-  }
-
-  get dc3() {
-    return this._dc3.map((task) => task._data);
-  }
-
-  get dc4() {
-    return this._dc4.map((task) => task._data);
-  }
-  get dc5() {
-    return this._dc5.map((task) => task._data);
-  }
-  get dc6() {
-    return this._dc6.map((task) => task._data);
-  }
-  get dc7() {
-    return this._dc7.map((task) => task._data);
-  }
-  get dc8() {
-    return this._dc8.map((task) => task._data);
-  }
-
-  addToDc1(task) {
-    this._dc1.push(task);
-  }
-
-  addToDc2(task) {
-    this._dc2.push(task);
-  }
-
-  addToDc3(task) {
-    this._dc3.push(task);
-  }
-  addToDc4(task) {
-    this._dc4.push(task);
-  }
-  addToDc5(task) {
-    this._dc5.push(task);
-  }
-  addToDc6(task) {
-    this._dc6.push(task);
-  }
-  addToDc7(task) {
-    this._dc7.push(task);
-  }
-  addToDc8(task) {
-    this._dc8.push(task);
+  getResolvedTasks() {
+    return this.tasks.filter((task) => task.State === "Resolved");
   }
 }
 
@@ -311,7 +330,7 @@ function setupEventListener() {
   });
 }
 
-setupEventListener();
+// setupEventListener();
 
 function sanitizeName(name) {
   return name.split(" <")[0].trim();
